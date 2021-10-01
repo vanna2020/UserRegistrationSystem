55b8785067f48038832e5a1118d98a449ec85990
vanna2020 <vannasingh1999@gmail.com>
2021-09-30 23:36:12 -0700

    enter a valid email address

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 
 
 public class UserRegistration {
-
     public static final Scanner scanner = new Scanner(System.in);
     /*
     Created a method to validate first name
@@ -27,7 +26,7 @@ public class UserRegistration {
     Created a method to validate last name
      */
     public static void validLastName(){
-        System.out.println("Enter the Last Name");
+        System.out.print("Enter the Last Name : ");
         String lname = scanner.next();
 
         String regex1 = "^[A-Z]{1}[a-z]{2,}$";
@@ -39,5 +38,21 @@ public class UserRegistration {
             System.out.println("Last name is Valid");
         else
             System.out.println("Last name is Invalid");
+    }
+    /*
+    Created a method to validate Email Address
+     */
+    public static void validEmailId(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Email Address : ");
+        String email = scanner.next();
+        String regex2 = "^[a-zA-Z]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
+        Pattern p2 = Pattern.compile(regex2);
+        Matcher m2 = p2.matcher(email);
+        boolean r2 = m2.matches();
+        if (r2)
+            System.out.println("Email Address is Valid");
+        else
+            System.out.println("Email Address is Invalid");
     }
 }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 
         public class UserRegistrationMain {
 
-            public static void welcome() {
+            public static void welcome(){
                 System.out.println("Welcome to User Registration System Problem");
             }
-
             public static void main(String[] args) {
                 welcome();
                 UserRegistration user = new UserRegistration();
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter 1 : to validate First Name ");
                 System.out.println("Enter 2 : to validate Last Name ");
-                switch (scanner.nextInt()) {
+                System.out.println("Enter 3 : to validate E-mail Address ");
+                switch (scanner.nextInt()){
                     case 1:
                         UserRegistration.validFirstName();
                         break;
                     case 2:
                         UserRegistration.validLastName();
+                        break;
+                    case 3:
+                        UserRegistration.validEmailId();
                         break;
                     default:
                         System.out.println("Select a valid number");
