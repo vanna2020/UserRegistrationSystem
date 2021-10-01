84f8daf8f54a2d7f41f96baed007cf6f91a7aac7
vanna2020 <vannasingh1999@gmail.com>
2021-10-01 00:56:07 -0700

    Enter valid password with atleast 1 Uppercase

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -5,86 +5,102 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class UserRegistration {
-    public static final Scanner scanner = new Scanner(System.in);
-    /*
-    Created a method to validate first name
-     */
-    public static void validFirstName(){
-        System.out.print("Enter the First Name : ");
-        String name = scanner.next();
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean r = m.matches();
-        if (r)
-            System.out.println("First name is valid");
-        else
-            System.out.println("First name is !Invalid");
-    }
-    /*
-    Created a method to validate last name
-     */
-    public static void validLastName(){
-        System.out.print("Enter the Last Name : ");
-        String lname = scanner.next();
+    public class UserRegistration {
+        public static final Scanner scanner = new Scanner(System.in);
+        /*
+        Created a method to validate first name
+         */
+        public static void validFirstName(){
+            System.out.print("Enter the First Name : ");
+            String name = scanner.next();
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(name);
+            boolean r = m.matches();
+            if (r)
+                System.out.println("First name is valid");
+            else
+                System.out.println("First name is !Invalid");
+        }
+        /*
+        Created a method to validate last name
+         */
+        public static void validLastName(){
+            System.out.print("Enter the Last Name : ");
+            String lname = scanner.next();
 
-        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
+            String regex1 = "^[A-Z]{1}[a-z]{2,}$";
 
-        Pattern p1 = Pattern.compile(regex1);
-        Matcher m1 = p1.matcher(lname);
-        boolean r1 = m1.matches();
-        if (r1)
-            System.out.println("Last name is Valid");
-        else
-            System.out.println("Last name is Invalid");
+            Pattern p1 = Pattern.compile(regex1);
+            Matcher m1 = p1.matcher(lname);
+            boolean r1 = m1.matches();
+            if (r1)
+                System.out.println("Last name is Valid");
+            else
+                System.out.println("Last name is Invalid");
+        }
+        /*
+        Created a method to validate Email Address
+         */
+        public static void validEmailId(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the Email Address : ");
+            String email = scanner.next();
+            String regex2 = "^[a-zA-Z]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
+            Pattern p2 = Pattern.compile(regex2);
+            Matcher m2 = p2.matcher(email);
+            boolean r2 = m2.matches();
+            if (r2)
+                System.out.println("Email Address is Valid");
+            else
+                System.out.println("Email Address is Invalid");
+        }
+        /*
+         Created a method to enter a valid mobile number
+         */
+        public static void validMobileNumber(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your mobile number : ");
+            String phoneNo = scanner.nextLine();
+            String regex3 = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
+            Pattern p3 = Pattern.compile(regex3);
+            Matcher m3 = p3.matcher(phoneNo);
+            boolean r3 = m3.matches();
+            if (r3)
+                System.out.println("Phone number is Valid");
+            else
+                System.out.println("Phone number is Invalid");
+        }
+        /*
+         Created a method to enter valid Password according to rule 1
+         */
+        public static void validPassRule1(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the Password according to rule 1 : ");
+            String passWord = scanner.nextLine();
+            String regex4 = "^[A-Z a-z 0-9]{8,}$";
+            Pattern p4 = Pattern.compile(regex4);
+            Matcher m4 = p4.matcher(passWord);
+            boolean r4 = m4.matches();
+            if (r4)
+                System.out.println("Password is Valid");
+            else
+                System.out.println("Password is Invalid");
+        }
+        /*
+        Created a method to enter valid Password according to rule 2
+        */
+        public static void validPassRule2(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the Password according to rule 2 : ");
+            String passWord1 = scanner.nextLine();
+            String regex5 = "^[A-Z]{1}+[a-zA-z0-9]{8,}$";
+            Pattern p5 = Pattern.compile(regex5);
+            Matcher m5 = p5.matcher(passWord1);
+            boolean r5 = m5.matches();
+            if (r5)
+                System.out.println("Password is Valid");
+            else
+                System.out.println("Password is Invalid");
+        }
     }
-    /*
-    Created a method to validate Email Address
-     */
-    public static void validEmailId(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Email Address : ");
-        String email = scanner.next();
-        String regex2 = "^[a-zA-Z]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
-        Pattern p2 = Pattern.compile(regex2);
-        Matcher m2 = p2.matcher(email);
-        boolean r2 = m2.matches();
-        if (r2)
-            System.out.println("Email Address is Valid");
-        else
-            System.out.println("Email Address is Invalid");
-    }
-    /*
-     User need to enter a valid mobile number
-     */
-    public static void validMobileNumber(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your mobile number : ");
-        String phoneNo = scanner.nextLine();
-        String regex3 = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
-        Pattern p3 = Pattern.compile(regex3);
-        Matcher m3 = p3.matcher(phoneNo);
-        boolean r3 = m3.matches();
-        if (r3)
-            System.out.println("Phone number is Valid");
-        else
-            System.out.println("Phone number is Invalid");
-    }
-    /*
-     User need to enter a valid Password according to rule 1
-     */
-    public static void validPassRule1(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Password according to rule 1 : ");
-        String passWord = scanner.nextLine();
-        String regex4 = "^[A-Z a-z 0-9]{8,}$";
-        Pattern p4 = Pattern.compile(regex4);
-        Matcher m4 = p4.matcher(passWord);
-        boolean r4 = m4.matches();
-        if (r4)
-            System.out.println("Password is Valid");
-        else
-            System.out.println("Password is Invalid");
-    }
-}
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -2,39 +2,42 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+public class UserRegistrationMain {
 
-        public class UserRegistrationMain {
-
-            public static void welcome(){
-                System.out.println("Welcome to User Registration System Problem");
-            }
-            public static void main(String[] args) {
-                welcome();
-                UserRegistration user = new UserRegistration();
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter 1 : to validate First Name ");
-                System.out.println("Enter 2 : to validate Last Name ");
-                System.out.println("Enter 3 : to validate E-mail Address ");
-                System.out.println("Enter 4 : to validate Mobile Number ");
-                System.out.println("Enter 5 : to validate Password Rule 1 ");
-                switch (scanner.nextInt()){
-                    case 1:
-                        UserRegistration.validFirstName();
-                        break;
-                    case 2:
-                        UserRegistration.validLastName();
-                        break;
-                    case 3:
-                        UserRegistration.validEmailId();
-                        break;
-                    case 4:
-                        UserRegistration.validMobileNumber();
-                        break;
-                    case 5:
-                        UserRegistration.validPassRule1();
-                        break;
-                    default:
-                        System.out.println("Select a valid number");
-                }
-            }
+    public static void welcome(){
+        System.out.println("Welcome to User Registration System Problem");
+    }
+    public static void main(String[] args) {
+        welcome();
+        UserRegistration user = new UserRegistration();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 : to validate First Name ");
+        System.out.println("Enter 2 : to validate Last Name ");
+        System.out.println("Enter 3 : to validate E-mail Address ");
+        System.out.println("Enter 4 : to validate Mobile Number ");
+        System.out.println("Enter 5 : to validate Password Rule 1 ");
+        System.out.println("Enter 6 : to validate Password Rule 2 ");
+        switch (scanner.nextInt()){
+            case 1:
+                UserRegistration.validFirstName();
+                break;
+            case 2:
+                UserRegistration.validLastName();
+                break;
+            case 3:
+                UserRegistration.validEmailId();
+                break;
+            case 4:
+                UserRegistration.validMobileNumber();
+                break;
+            case 5:
+                UserRegistration.validPassRule1();
+                break;
+            case 6:
+                UserRegistration.validPassRule2();
+                break;
+            default:
+                System.out.println("Select a valid number");
         }
+    }
+}
