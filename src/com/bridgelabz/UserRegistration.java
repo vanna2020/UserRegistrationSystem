042bc9a39487ef2b12e18a484bb7181556716177
vanna2020 <vannasingh1999@gmail.com>
2021-10-01 00:34:27 -0700

    User need to enter a password with minimum 8 character

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -56,7 +56,7 @@ public class UserRegistration {
             System.out.println("Email Address is Invalid");
     }
     /*
-     Created a method to validate Mobile Number
+     User need to enter a valid mobile number
      */
     public static void validMobileNumber(){
         Scanner scanner = new Scanner(System.in);
@@ -70,5 +70,21 @@ public class UserRegistration {
             System.out.println("Phone number is Valid");
         else
             System.out.println("Phone number is Invalid");
+    }
+    /*
+     User need to enter a valid Password according to rule 1
+     */
+    public static void validPassRule1(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Password according to rule 1 : ");
+        String passWord = scanner.nextLine();
+        String regex4 = "^[A-Z a-z 0-9]{8,}$";
+        Pattern p4 = Pattern.compile(regex4);
+        Matcher m4 = p4.matcher(passWord);
+        boolean r4 = m4.matches();
+        if (r4)
+            System.out.println("Password is Valid");
+        else
+            System.out.println("Password is Invalid");
     }
 }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -16,6 +16,7 @@ import java.util.Scanner;
                 System.out.println("Enter 2 : to validate Last Name ");
                 System.out.println("Enter 3 : to validate E-mail Address ");
                 System.out.println("Enter 4 : to validate Mobile Number ");
+                System.out.println("Enter 5 : to validate Password Rule 1 ");
                 switch (scanner.nextInt()){
                     case 1:
                         UserRegistration.validFirstName();
@@ -28,6 +29,9 @@ import java.util.Scanner;
                         break;
                     case 4:
                         UserRegistration.validMobileNumber();
+                        break;
+                    case 5:
+                        UserRegistration.validPassRule1();
                         break;
                     default:
                         System.out.println("Select a valid number");
